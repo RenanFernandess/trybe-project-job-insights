faa82f97a2eb05be23fef2434ec3b16e755dc19b
Renan Fernandes <renzinestuods@gmail.com>
2023-03-28 18:26:18 -0300

    :white_check_mark: test: Testa se read_brazilian_file retorna os jobs tradusidos para o inglês

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,27 @@
 # from src.pre_built.brazilian_jobs import read_brazilian_file
+import pytest
+from unittest.mock import patch, Mock
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
-def test_brazilian_jobs():
-    pass
+@pytest.fixture
+def brazilian_jobs():
+    return [
+        {"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"},
+        {"titulo": "Motorista", "salario": "3000", "tipo": "full time"},
+    ]
+
+
+@pytest.fixture
+def brazilian_jobs_translated():
+    return [
+        {"title": "Maquinista", "salary": "2000", "type": "trainee"},
+        {"title": "Motorista", "salary": "3000", "type": "full time"},
+    ]
+
+
+def test_brazilian_jobs(brazilian_jobs, brazilian_jobs_translated):
+    "Testa se read_brazilian_file retorna os jobs tradusidos para o inglês"
+    with patch("src.insights.jobs.read", Mock(return_value=brazilian_jobs)):
+        assert type(read_brazilian_file("fake_path")) == list
+        assert read_brazilian_file("fake_path") == brazilian_jobs_translated
